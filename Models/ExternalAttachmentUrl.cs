2637f9cc09693e84308d677662714888b5991a39
lctech-roy <Roy@lctech.com.tw>
2023-02-20 16:24:18 +0800

    remove ExternalAttachmentUrl

--- Models/ExternalAttachmentUrl.cs
+++
@@ -1,9 +0,0 @@
-namespace ForumDataMigration.Models;
-
-public class ExternalAttachmentUrl
-{
-    public long AttachmentId { get; set; }
-    public int Tid { get; set; }
-    public int Pid { get; set; }
-    public string? AttachmentUrl { get; set; }
-}
