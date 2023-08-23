06dd625c5970d2c9ed198070dcb24519842b7553
lctech-roy <Roy@lctech.com.tw>
2023-08-23 15:42:55 +0800

    feat: query new forum member change to old forum

--- Helpers/MemberHelper.cs
+++ Helpers/MemberHelper.cs
@@ -1,5 +1,6 @@
 using Dapper;
 using ForumDataMigration.Helper;
+using MySqlConnector;
 using Npgsql;
 
 namespace ForumDataMigration.Helpers;
@@ -8,12 +9,14 @@ public class MemberHelper
 {
     public static Dictionary<string, long> GetMemberNameDic()
     {
-        const string sql = @"SELECT ""Id"",""Value"" FROM ""MemberExtendData"" med WHERE ""Key"" = 'OriginUsername'";
+        const string sql = @"SELECT uid,username FROM pre_ucenter_members 
+                             UNION ALL
+                             SELECT uid,username FROM pre_common_member WHERE uid NOT IN (SELECT uid FROM pre_ucenter_members)";
 
         var dic = CommonHelper.WatchTime(nameof(GetMemberNameDic)
                                        , () =>
                                          {
-                                             using var conn = new NpgsqlConnection(Setting.NEW_MEMBER_CONNECTION);
+                                             using var conn = new MySqlConnection(Setting.OLD_FORUM_CONNECTION);
 
                                              var idDic = conn.Query<(long memberId, string userName)>(sql).ToDictionary(t => t.userName, t => t.memberId);
 
@@ -28,14 +31,16 @@ public class MemberHelper
     ///</summary>
     public static HashSet<long> GetProhibitMemberIdHash()
     {
-        const string sql = @"SELECT m.""Id"" FROM ""MemberGroup"" mg 
-        INNER JOIN ""Member"" m ON m.""Id""  = mg.""Id"" 
-        WHERE ""GroupId"" = 5 OR ""GroupId"" = 4";
+        const string sql = @"SELECT @dateNow := UNIX_TIMESTAMP(NOW());
+                             SELECT uid FROM pre_common_member WHERE 
+                             (groupid = 4 AND (groupexpiry = 0 OR groupexpiry > @dateNow)) OR 
+                             (groupid = 5 AND (groupexpiry = 0 OR groupexpiry > @dateNow)) OR
+                             (extgroupids = '4') OR (extgroupids = '5')";
 
         var hashSet = CommonHelper.WatchTime(nameof(GetProhibitMemberIdHash)
                                        , () =>
                                          {
-                                             using var conn = new NpgsqlConnection(Setting.NEW_MEMBER_CONNECTION);
+                                             using var conn = new MySqlConnection(Setting.OLD_FORUM_CONNECTION);
 
                                              var idDic = conn.Query<long>(sql).ToHashSet();
 
